e34ac4adea83d151ccf8f5d6afa5200678a38118
Sergey Boltonosov <minstel@yandex.ru>
2018-06-19 19:13:43 +0400

    Prepare for switching between multiple scripts

--- run.js
+++ run.js
@@ -1,13 +1,20 @@
 'use strict'
 
-const func = require('./index.js');
-const params = {
-    walletSeed: process.env.SOURCE_WALLET_SEED,
-    recipient: process.env.RECIPIENT_WALLET_ADRESS,
-    tokenName: process.env.TOKEN_NAME,
-    tokenDesc: process.env.TOKEN_DESC,
-    tokenQuantity: process.env.TOKEN_QUANTITY,
-    wavesAmount: process.env.TRANSACTION_WAVES_AMOUNT
-};
+const type = process.env.RUN_TYPE;
 
-func(params);
+console.log('run script: ', type, "\n");
+
+switch(type) {
+    case 'wavesTransaction':
+        const wavesTransaction = require('./scripts/waves-transaction.js');
+        const params = {
+            walletSeed: process.env.SOURCE_WALLET_SEED,
+            recipient: process.env.RECIPIENT_WALLET_ADRESS,
+            tokenName: process.env.TOKEN_NAME,
+            tokenDesc: process.env.TOKEN_DESC,
+            tokenQuantity: process.env.TOKEN_QUANTITY,
+            wavesAmount: process.env.TRANSACTION_WAVES_AMOUNT        
+        };
+
+        wavesTransaction(params);
+}
