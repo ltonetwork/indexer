2f813343def23b07361ceae777fad969bd6b735c
Moesjarraf J <moesjarraf@gmail.com>
2018-09-26 15:37:56 +0200

    Fix e2e test for info endpoint (#14)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,9 @@
 import { Get, Controller, Res } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ApiOperation, ApiResponse, ApiUseTags } from '@nestjs/swagger';
-import { Response } from "express";
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor() { }
 
   @Get()
   async root(@Res() res: Response): Promise<void> {
