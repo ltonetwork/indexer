03ca5cba97233a9a73dba178f6818b42944de99c
sven4ask <sven@legalthings.io>
2018-09-26 14:27:43 +0200

    Move info to info endpoint and redirect to api docs

---
+++ src/info/info.controller.spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { InfoController } from './info.controller';
+import { InfoService } from './info.service';
+import { ConfigModule } from '../config/config.module';
+
+describe('InfoController', () => {
+  let module: TestingModule;
+  let infoController: InfoController;
+  let infoService: InfoService;
+
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [ConfigModule],
+      controllers: [InfoController],
+      providers: [InfoService],
+    }).compile();
+    module.init();
+
+    infoService = module.get<InfoService>(InfoService);
+    infoController = module.get<InfoController>(InfoController);
+  });
+
+  afterEach(async () => {
+    await module.close();
+  });
+
+  describe('info', () => {
+    test('should return application info', async () => {
+      const result = { name: 'foo' };
+      jest.spyOn(infoService, 'info').mockImplementation(() => result);
+      expect(await infoController.info()).toBe(result);
+    });
+  });
+});
