8d120a3470c3fbcc94e2f4d39ac009b886cb99c9
sven4ask <sven@legalthings.io>
2018-09-27 10:47:10 +0200

    Set default value hash dto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,6 @@ import { Get, Controller, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
-
 @Controller()
 export class AppController {
   constructor() { }
