d12e67c637ed49eede4910c974e8ac3d417e180b
sven4ask <sven@legalthings.io>
2018-09-27 10:40:09 +0200

    Remove root from swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Get, Controller, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
+
 
 @Controller()
 export class AppController {
   constructor() { }
 
   @Get()
+  @ApiExcludeEndpoint()
   async root(@Res() res: Response): Promise<void> {
     return res.redirect('/api-docs');
   }
