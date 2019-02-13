2cbca293fc39ab8f97afe818f215d65ec4de8fd4
Yevhen Horbachov <killabee1985@gmail.com>
2019-02-13 12:59:56 +0100

    add file-drop component (#37)

--- frontend/src/app/components/file-input/file-input.component.ts
+++ frontend/src/app/components/file-input/file-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { sha256 } from 'js-sha256';
+import { UploadEvent, FileSystemFileEntry } from 'ngx-file-drop';
 import { EncoderService } from '../../encoder.service';
 
 @Component({
@@ -29,8 +30,22 @@ export class FileInputComponent implements OnInit {
     this._readFile(file);
   }
 
+  dropped(event: UploadEvent) {
+    const file = event.files[0].fileEntry as FileSystemFileEntry;
+
+    file.file((f: File) => {
+      this._readFile(f);
+    });
+  }
+
+  fileSelected(event: any) {
+    const file = event.target.files[0];
+    this._readFile(file);
+  }
+
   private _readFile(file: File) {
     const reader = new FileReader();
+    this.fileName = file.name;
     reader.onload = () => {
       this._generateHash(reader.result);
       this.fileData.next(reader.result);
