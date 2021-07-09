5c3f302cb27f286fa068bdae97092fe672dca2d9
Bruno Brito <brunodb3@gmail.com>
2021-07-09 11:28:28 +0200

    Initial TODOs

---
+++ src/supply/supply.service.ts
@@ -0,0 +1,16 @@
+// @todo: What is supply:
+//        total supply (balance of all addresses?)
+//        minus balance of burn wallets
+//        minus amount burned in ERC20
+//        minus TX fee burn
+
+// @todo: For the total supply
+//        need to look up the list of addresses
+//        then can get balance from the public node
+
+// @todo: ERC20 is a fixed amount (?)
+//        ERC20 is the Ethereum token
+
+// @todo: For fee burn:
+//        count number of txs since block where feature activated * 0.1 LTO (tx fee burn)
+//        the fee burn for each tx changes to a percentage with the new feature vote of v1.4
