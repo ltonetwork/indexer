e83461312234e0cbd9cebe90725a2ae8a18e9fb4
Bruno Brito <brunodb3@gmail.com>
2021-07-16 16:23:58 +0200

    Adding config for choosing anchor indexing

--- src/anchor/anchor.service.spec.ts
+++ src/anchor/anchor.service.spec.ts
@@ -2,18 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AnchorModuleConfig } from './anchor.module';
 import { AnchorService } from './anchor.service';
 import { StorageService } from '../storage/storage.service';
+import { ConfigService } from '../config/config.service';
 
 describe('AnchorService', () => {
   let module: TestingModule;
   let anchorService: AnchorService;
+  let configService: ConfigService;
   let storageService: StorageService;
 
   function spy() {
     const storage = {
       saveAnchor: jest.spyOn(storageService, 'saveAnchor').mockImplementation(async () => { }),
+      getRolesFor: jest.spyOn(storageService, 'getRolesFor').mockImplementation(async () => { return {} }),
     };
 
-    return { storage };
+    const config = {
+      getAnchorIndexing: jest.spyOn(configService, 'getAnchorIndexing').mockImplementation(() => 'all'),
+    };
+
+    return { storage, config };
   }
 
   beforeEach(async () => {
@@ -21,6 +28,7 @@ describe('AnchorService', () => {
     await module.init();
 
     anchorService = module.get<AnchorService>(AnchorService);
+    configService = module.get<ConfigService>(ConfigService);
     storageService = module.get<StorageService>(StorageService);
   });
 
@@ -75,8 +83,53 @@ describe('AnchorService', () => {
         .toMatchObject({ id: 'fake_transaction', blockHeight: 1, position: 0 });
     });
 
-    test.skip('should not process a non anchor transaction', async () => {
+    test('should not process when config says "none"', async () => {
+      const spies = spy();
 
+      spies.config.getAnchorIndexing = jest.spyOn(configService, 'getAnchorIndexing').mockImplementation(() => 'none');
+
+      const transaction = {
+        id: 'fake_transaction',
+        type: 15,
+        anchors: [
+          '3zLWTHPNkmDsCRi2kZqFXFSBnTYykz13gHLezU4p6zmu',
+        ],
+      };
+
+      await anchorService.index({transaction: transaction as any, blockHeight: 1, position: 0});
+
+      expect(spies.storage.saveAnchor.mock.calls.length).toBe(0);
+    });
+
+    test('should only process "trust" anchors when configured', async () => {
+      const spies = spy();
+
+      spies.config.getAnchorIndexing = jest.spyOn(configService, 'getAnchorIndexing').mockImplementation(() => 'trust');
+      spies.storage.getRolesFor = jest.spyOn(storageService, 'getRolesFor').mockImplementation(async (address: string) => {
+        if (address === '3Mv7ajrPLKewkBNqfxwRZoRwW6fziehp7dQ') {
+          return { root: { description: 'The root role' } };
+        }
+
+        return {};
+      });
+
+      const transaction = {
+        id: 'fake_transaction',
+        type: 15,
+        sender: '3JuijVBB7NCwCz2Ae5HhCDsqCXzeBLRTyeL',
+        anchors: [
+          '3zLWTHPNkmDsCRi2kZqFXFSBnTYykz13gHLezU4p6zmu',
+        ],
+      };
+
+      await anchorService.index({transaction: transaction as any, blockHeight: 1, position: 0});
+
+      transaction.id = 'second_fake_transaction';
+      transaction.sender = '3Mv7ajrPLKewkBNqfxwRZoRwW6fziehp7dQ';
+
+      await anchorService.index({transaction: transaction as any, blockHeight: 2, position: 1});
+
+      expect(spies.storage.saveAnchor.mock.calls.length).toBe(1);
     });
   });
 });
