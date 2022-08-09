975b620f191cdb50b29e20fcadb0963ebc4236bf
arnold <arnold@jasny.net>
2022-08-09 11:27:49 -0400

    Leveldb async lock per key instead of global.

--- src/leveldb/classes/leveldb.connection.ts
+++ src/leveldb/classes/leveldb.connection.ts
@@ -6,10 +6,24 @@ import AwaitLock from 'await-lock';
  * @todo Move this logic into leveldb.storage.service
  */
 export class LeveldbConnection {
-  private incrLock: AwaitLock;
+  private incrLock: {[key: string]: AwaitLock} = {};
 
-  constructor(private connection: level.Level) {
-    this.incrLock = new AwaitLock();
+  constructor(private connection: level.Level) {}
+
+  private async lock(key: string) {
+    if (!this.incrLock.hasOwnProperty(key)) {
+      this.incrLock[key] = new AwaitLock();
+    }
+
+    return await this.incrLock[key].acquireAsync();
+  }
+
+  private unlock(key: string) {
+    this.incrLock[key].release();
+
+    if (!this.incrLock[key].acquired) {
+      delete this.incrLock[key];
+    }
   }
 
   get(key: level.KeyType): Promise<string> {
@@ -28,7 +42,7 @@ export class LeveldbConnection {
   }
 
   async add(key: level.KeyType, value: string): Promise<string> {
-    await this.incrLock.acquireAsync();
+    await this.lock(key);
 
     try {
       const existing = await this.get(key);
@@ -38,7 +52,7 @@ export class LeveldbConnection {
       const result = await this.connection.put(key, value);
       return result;
     } finally {
-      this.incrLock.release();
+      this.unlock(key);
     }
   }
 
@@ -51,14 +65,14 @@ export class LeveldbConnection {
   }
 
   async incr(key, amount = 1): Promise<string> {
-    await this.incrLock.acquireAsync();
+    await this.lock(key);
 
     try {
       const count = Number(await this.get(key));
       const result = await this.set(key, String(count + amount));
       return result;
     } finally {
-      this.incrLock.release();
+      this.unlock(key);
     }
   }
 
