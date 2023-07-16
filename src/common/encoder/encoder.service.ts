111b047a1647a2c3ae060139790426587ac40746
arnold <arnold@jasny.net>
2023-07-16 00:36:26 -0400

    Added TODO to remove EncoderService

--- src/common/encoder/encoder.service.ts
+++ src/common/encoder/encoder.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
+// TODO: Remove this service, just use the @scure/base package
+
 @Injectable()
 export class EncoderService {
   private alphabet: string;
